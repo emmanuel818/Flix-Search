3c66bf85cca0e9619aea33415c0c52f38ee20ee0
emmanuel818 <emmanuelg8182001@gmail.com>
2021-10-20 14:48:16 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -190,11 +190,11 @@ app.put('/users/:Username', passport.authenticate('jwt', {session: false}),
   if(!errors.isEmpty()) {
     return res.status(422).json({errors: errors.array() });
   }
-
+  let hashedPassword = Users.hashPassword(req.body.Password)
   Users.findOneAndUpdate({ Username: req.params.Username}, {$set: 
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Eamil: req.body.Email,
       Birthday: req.body.Birthday
     }

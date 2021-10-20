47b50feb4519d372b7211068ae90f89e1d31742d
emmanuel818 <emmanuelg8182001@gmail.com>
2021-10-20 15:36:02 -0500

    Updated password to be hashed upom update of user

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.put('/users/:Username', passport.authenticate('jwt', {session: false}),
   Users.findOneAndUpdate({ Username: req.params.Username}, {$set: 
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }

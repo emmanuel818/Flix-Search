bfbeecd32c4035ba2f4198dc1bb696d4c1bc0a0a
Emmanuel Gonzalez <85756089+emmanuel818@users.noreply.github.com>
2022-02-28 12:27:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http:localhost8080', 'http://localhost:1234', 'http:/testsite.com'];
+let allowedOrigins = ['http:localhost8080', 'http://localhost:1234', 'http:/testsite.com', 'http://localhost:4200/'];
 
 app.use(cors({
   origin: (origin, callback) => {
